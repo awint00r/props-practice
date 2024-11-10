d5bc809040374c7fb98a190c7625930e81ae04ee
awint00r <austinwinterberg@yahoo.com>
2024-11-10 11:49:26 -0600

    Added export and adjusted onClick for toggleLikes.

--- src/PostActions.js
+++ src/PostActions.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react' 
 
-// Refer to the module pattern we reviewed several times in class today and debug the error of this component.
-
 class PostActions extends Component {
     
     render() {
         return (
         <>
-            <button onClick={this.props.onLike} className="like-button">
+            <button onClick={this.props.toggleLike} className="like-button">
                 <i className={this.props.liked ? 'fas fa-heart' : 'far fa-heart'}></i>
             </button>
             <p>{this.props.totalLikes}</p>
@@ -22,4 +20,4 @@ class PostActions extends Component {
     }
 }
 
-
+export default PostActions;
