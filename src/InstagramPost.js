e0f177454efce07daa4f37de2b81e6507577686f
awint00r <austinwinterberg@yahoo.com>
2024-11-10 10:02:00 -0600

    Declared local state keys with correct values.

--- src/InstagramPost.js
+++ src/InstagramPost.js
@@ -6,7 +6,14 @@ class InstagramPost extends Component {
     constructor(props) {
         super(props);
         this.state = {
-           // Task 4: Declare local state keys and assign them the value correlating to the props parameter of the constructor. For example, potato: props.potato
+            liked: props.liked,
+            saved: props.saved,
+            totalLikes: props.totalLikes,
+            caption: props.caption,
+            userName: props.userName,
+            postImgURL: props.postImgURL,
+            userImgURL: props.userImgURL,
+            comments: props.comments,
            newComment: ''
         };
     }
